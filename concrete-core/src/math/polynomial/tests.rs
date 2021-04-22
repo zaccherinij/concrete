287299ae4a4e019f5791a4e0b83c5cbeebc4df2d
Damien Ligier <damien.ligier@gmail.com>
2021-04-22 11:52:29 +0200

    test(concrete-core/math/polynomial): add karatsuba multiplication (zama-ai/concrete_internal#34)

--- concrete-core/src/math/polynomial/tests.rs
+++ concrete-core/src/math/polynomial/tests.rs
@@ -1,5 +1,5 @@
 use crate::crypto::UnsignedTorus;
-use crate::math::polynomial::{MonomialDegree, Polynomial};
+use crate::math::polynomial::{MonomialDegree, Polynomial, PolynomialSize};
 use crate::math::random;
 use rand::Rng;
 
@@ -48,6 +48,36 @@ fn test_multiply_divide_unit_monomial<T: UnsignedTorus>() {
     assert_eq!(&poly, &ground_truth);
 }
 
+fn test_multiply_karatsuba<T: UnsignedTorus>() {
+    //! tests if multiply_by_monomial and divide_by_monomial cancel each other
+    let mut rng = rand::thread_rng();
+
+    // settings
+    let polynomial_log = (rng.gen::<usize>() % 7) + 6;
+    let polynomial_size = PolynomialSize(1 << polynomial_log);
+
+    // generates two random Torus polynomials
+    let poly_1 = Polynomial::from_container(
+        random::random_uniform_tensor::<T>(polynomial_size.0).into_container(),
+    );
+    let poly_2 = Polynomial::from_container(
+        random::random_uniform_tensor::<T>(polynomial_size.0).into_container(),
+    );
+
+    // copy this polynomial
+    let mut sb_mul = Polynomial::allocate(T::ZERO, polynomial_size);
+    let mut ka_mul = Polynomial::allocate(T::ZERO, polynomial_size);
+
+    // compute the schoolbook
+    sb_mul.fill_with_wrapping_mul(&poly_1, &poly_2);
+
+    // compute the karatsuba
+    ka_mul.fill_with_karatsuba_mul(&poly_1, &poly_2);
+
+    // test
+    assert_eq!(&sb_mul, &ka_mul);
+}
+
 #[test]
 pub fn test_multiply_divide_unit_monomial_u32() {
     test_multiply_divide_unit_monomial::<u32>()
@@ -57,3 +87,13 @@ pub fn test_multiply_divide_unit_monomial_u32() {
 pub fn test_multiply_divide_unit_monomial_u64() {
     test_multiply_divide_unit_monomial::<u64>()
 }
+
+#[test]
+pub fn test_multiply_karatsuba_u32() {
+    test_multiply_karatsuba::<u32>()
+}
+
+// #[test]
+// pub fn test_multiply_karatsuba_u64() {
+//     test_multiply_karatsuba::<u64>()
+// }
