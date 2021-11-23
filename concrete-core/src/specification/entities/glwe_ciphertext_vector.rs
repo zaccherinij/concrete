03a0a14378a6a3f6e8ea9e53533eea6b3aa12bc2
Alexandre Péré <alexandre.pere@zama.ai>
2021-11-23 09:54:48 +0100

    fix(concrete_core): change KeyFlavor for KeyDistribution
    BREAKING CHANGE: In `concrete_core` the _distribution_ word is used in place of the _flavor_ word.

--- concrete-core/src/specification/entities/glwe_ciphertext_vector.rs
+++ concrete-core/src/specification/entities/glwe_ciphertext_vector.rs
@@ -1,20 +1,20 @@
-use crate::specification::entities::markers::{GlweCiphertextVectorKind, KeyFlavorMarker};
+use crate::specification::entities::markers::{GlweCiphertextVectorKind, KeyDistributionMarker};
 use crate::specification::entities::AbstractEntity;
 use concrete_commons::parameters::{GlweCiphertextCount, GlweDimension, PolynomialSize};
 
 /// A trait implemented by types embodying a GLWE ciphertext vector.
 ///
 /// A GLWE ciphertext vector is associated with a
-/// [`KeyFlavor`](`GlweCiphertextVectorEntity::KeyFlavor`) type, which conveys the flavor of secret
-/// key it was encrypted with.
+/// [`KeyDistribution`](`GlweCiphertextVectorEntity::KeyDistribution`) type, which conveys the
+/// distribution of the secret key it was encrypted with.
 ///
 /// # Formal Definition
 ///
 /// Despite the fact that the GLWE scheme theoretically encompass the LWE scheme, in `concrete`, we forbid the
 /// use of GLWE ciphertext entities to represent encryption of polynomials of size zero.
 pub trait GlweCiphertextVectorEntity: AbstractEntity<Kind = GlweCiphertextVectorKind> {
-    /// The flavor of key the ciphertext was encrypted with.
-    type KeyFlavor: KeyFlavorMarker;
+    /// The distribution of the key the ciphertext was encrypted with.
+    type KeyDistribution: KeyDistributionMarker;
 
     /// Returns the GLWE dimension of the ciphertexts.
     fn glwe_dimension(&self) -> GlweDimension;
